03ef4c28ac076266005d3d5aa868f1905bfbee23
yumauma20 <71yuma10@gmail.com>
2020-02-10 01:15:49 +0900

    UIの作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="app-layout">
+    <div class="sidebar">
+      <p>チャンネル一覧</p>
+      <p>#general</p>
+      <p>#random</p>
+      <p>#random</p>
+    </div>
+    <div class="main-content">
+      <nuxt />
+    </div>
   </div>
 </template>
 
 <style>
+.app-layout {
+  display: flex;
+}
+
+.sidebar {
+  width: 300px;
+  background: #4a4141;
+  height: 100vh;
+  padding: 20px;
+}
+
+.sidebar p {
+  color: #DDDDDD;
+  padding-top:4px;
+}
+
+.main-content {
+  width: 100%;
+  background: #f1f1f1;
+  height: 100vh;
+}
+
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
